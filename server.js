dbd694b6ae6d7c797921d8c100e229a27b9e7781
SteveMc <contact@stevemcarthur.co.uk>
2019-02-01 16:17:09 +0000

    config change

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 var app = express();
 
 //app configuration
-var ipaddr = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddr = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 
 //mysql configuration
